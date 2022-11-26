37a51bf084aa6d63ba0eea5da14ca45799b676f3
kennedy maina <kenneyrit@gmail.com>
2022-11-26 21:42:41 +0300

    commit tree 70629101b60190580471806e0083892b8eb42c50

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someNumber <= 400 ) {
+    return "This one is on me!"; 
+} else if (someNumber > 2000 && someNumber <= 2500) {
+  return "I will gladly take your thirty bucks.";
+} else if (someNumber > 2500) {
+  return "No can do.";
+} else {
+  return "No ride available";
+}
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (city === "NYC"){
+    return "Ok, sounds good."
+   }
+   else{
+    return "No go.";
+   }
+  }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous"){
+    return "Thank you so much."
+  }else if (tip === "not as generous"){
+    return "Thank you."
+  }else {
+    return "Bye."
+  }
 }
